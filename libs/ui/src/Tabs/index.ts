26df46b3afcac81cc1758930b42edb89817405f2
Gonzalo Chumillas <gonzalo.chumillas@hyve.net>
2024-10-21 22:58:13 +0100

    update

--- libs/ui/src/Tabs/index.ts
+++ libs/ui/src/Tabs/index.ts
@@ -1,2 +1,8 @@
+/**
+ * Entry point.
+ * 
+ * Export the default component, functions and relevant types to the rest of
+ * the application.
+ */
 export { default } from './Tabs'
 export type { TabsItemProps, TabsProps } from './types'
